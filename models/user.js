07091c995e6a8e6be626c7d1abdf896656ceedf4
krasotun <servrostov@gmail.com>
2022-08-20 00:12:21 +0300

    add Начал работу

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(v) {
+        return isEmail(v);
+      },
+      message: (props) => `${props.value} не является email`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
