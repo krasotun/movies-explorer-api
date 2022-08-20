2bbe12825ef980f93783fb43d93cc3c67bd03aaf
Marat <servrostov@gmail.com>
2022-08-20 15:02:49 +0300

    add Настроил joi для создания пользователей

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const { errors, celebrate, Joi } = require('celebrate');
+const { errors, celebrate, Joi } = require('celebrate');
 const { PORT, DB_ADRESS } = require('./utils/constants');
 const { createUser } = require('./controllers/users');
 
@@ -12,7 +12,19 @@ mongoose.connect(DB_ADRESS, {
 
 app.use(express.json());
 
-app.post('/signup', createUser);
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+      name: Joi.string().min(2).max(30),
+    }),
+  }),
+  createUser,
+);
+
+app.use(errors());
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
