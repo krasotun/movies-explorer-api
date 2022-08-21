db00d9cf3b2e86a1f18bcbf959fbc3604cf15d27
Marat <servrostov@gmail.com>
2022-08-21 16:27:21 +0300

    add Добавил модель movie

---
+++ models/movie.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+const isURL = require('validator/lib/isURL');
+const { MOVIE_SCHEMA_REQ_MSGS, MOVIE_SCHEMA_VAL_MSGS } = require('../utils/constants');
+
+const movieSchema = new mongoose.Schema({
+
+  country: {
+    type: String,
+    required: [true, MOVIE_SCHEMA_REQ_MSGS.COUNTRY],
+  },
+  director: {
+    type: String,
+    required: [true, MOVIE_SCHEMA_REQ_MSGS.DIRECTOR],
+  },
+  duration: {
+    type: Number,
+    required: [true, MOVIE_SCHEMA_REQ_MSGS.DURATION],
+  },
+  year: {
+    type: String,
+    required: [true, MOVIE_SCHEMA_REQ_MSGS.YEAR],
+  },
+  description: {
+    type: String,
+    required: [true, MOVIE_SCHEMA_REQ_MSGS.DESCRIPTION],
+  },
+  image: {
+    type: String,
+    required: [true, MOVIE_SCHEMA_REQ_MSGS.IMAGE],
+    validate: {
+      validator(v) {
+        return isURL(v);
+      },
+      message: (props) => `${props.value} ${MOVIE_SCHEMA_VAL_MSGS.IMAGE_LINK}`,
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: [true, MOVIE_SCHEMA_REQ_MSGS.TRAILER],
+    validate: {
+      validator(v) {
+        return isURL(v);
+      },
+      message: (props) => `${props.value} ${MOVIE_SCHEMA_VAL_MSGS.TRAILER_LINK}`,
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: [true, MOVIE_SCHEMA_REQ_MSGS.THUMBNAIL],
+    validate: {
+      validator(v) {
+        return isURL(v);
+      },
+      message: (props) => `${props.value} ${MOVIE_SCHEMA_VAL_MSGS.THUMBNAIL_LINK}`,
+    },
+  },
+  owner: {
+    required: [true, MOVIE_SCHEMA_REQ_MSGS.OWNER],
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  movieId: {
+    type: Number,
+    required: [true, MOVIE_SCHEMA_REQ_MSGS.MOVIEID],
+  },
+
+  nameRU: {
+    type: String,
+    required: [true, MOVIE_SCHEMA_REQ_MSGS.NAMERU],
+  },
+  nameEN: {
+    type: String,
+    required: [true, MOVIE_SCHEMA_REQ_MSGS.NAMERU],
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,6 @@
 const PORT = 3000;
 const DB_ADDRESS = 'mongodb://localhost:27017/diploma';
+
 const USER_SCHEMA_REQ_MSGS = {
   EMAIL: 'Электронная почта (e-mail)  явлется обязательным полем для заполнения',
   PASSWORD: 'Пароль (password) явлется обязательным полем для заполнения',
@@ -9,6 +10,27 @@ const USER_SCHEMA_VAL_MSGS = {
   EMAIL: 'Введенное значение не явлется корректным адресом e-mail',
   NAME: 'Необходимая длина от 2 до 30 символов',
 };
+
+const MOVIE_SCHEMA_REQ_MSGS = {
+  COUNTRY: 'Страна (country)  явлется обязательным полем для заполнения',
+  DIRECTOR: 'Режиссер (director) явлется обязательным полем для заполнения',
+  DURATION: 'Длительность фильма (duration) явлется обязательным полем для заполнения',
+  YEAR: 'Год выпуска фильма (year) явлется обязательным полем для заполнения',
+  DESCRIPTION: 'Описание фильма (description) явлется обязательным полем для заполнения',
+  IMAGE: 'Постер фильма (image) явлется обязательным полем для заполнения',
+  TRAILER: 'Ссылка на трейлер фильма (trailerLink) явлется обязательным полем для заполнения',
+  THUMBNAIL: 'Миниатюрное изображение постера к фильму (thumbnail) явлется обязательным полем для заполнения',
+  OWNER: 'id пользователя, который сохранил фильм (owner) явлется обязательным полем для заполнения',
+  MOVIEID: 'id фильма (movieId) явлется обязательным полем для заполнения',
+  NAMERU: 'Название фильма на русском языке(nameRU) явлется обязательным полем для заполнения',
+  NAMEEN: 'Название фильма на английском языке(nameEN) явлется обязательным полем для заполнения',
+};
+const MOVIE_SCHEMA_VAL_MSGS = {
+  IMAGE_LINK: 'Не является корректным URL адресом для постера к фильму',
+  TRAILER_LINK: 'Не является корректным URL адресом для трейлера к фильму',
+  THUMBNAIL_LINK: 'Не является корректным URL адресом для миниатюрного изображения к фильму',
+};
+
 const BAD_REQ_MSG = 'Данные переданы неверно';
 const DUPLICATE_DATA_MSG = 'Данные уже есть в базе данных';
 const AUTH_ERR_MSG = 'Неверный e-mail или пароль';
@@ -21,6 +43,8 @@ module.exports = {
   DB_ADDRESS,
   USER_SCHEMA_REQ_MSGS,
   USER_SCHEMA_VAL_MSGS,
+  MOVIE_SCHEMA_REQ_MSGS,
+  MOVIE_SCHEMA_VAL_MSGS,
   BAD_REQ_MSG,
   DUPLICATE_DATA_MSG,
   AUTH_ERR_MSG,
